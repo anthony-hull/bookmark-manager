8999dfc4ddda3b06e451d0e03b09d6dd72b5f2d7
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-06-04 14:10:11 +0100

    changed the prep db method to spec helper before each

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:each) { prep_test_db }
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
